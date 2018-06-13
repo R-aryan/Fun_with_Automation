6d92962de4829a886690484bbe3c6c7554ec9159
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-13 14:50:51 +0530

    setting up timer

--- website_blocker.py
+++ website_blocker.py
@@ -1,4 +1,10 @@
+import time
 
 hosts_path= r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","fb.com","facebook.com","www.hotmail.com"]
+
+while True:
+    print(1)
+    time.sleep(5)
+
