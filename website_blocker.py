d55759ad4869f8f7238ad1c28b7d6414f7fb1a2f
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-13 16:06:09 +0530

    scheduling time

--- website_blocker.py
+++ website_blocker.py
@@ -1,10 +1,16 @@
 import time
+from datetime import datetime as dt
 
 hosts_path= r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","fb.com","facebook.com","www.hotmail.com"]
 
 while True:
-    print(1)
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16) :
+        print("working hours..")
+    
+    else:
+        print("fun hours....")
     time.sleep(5)
+
 
