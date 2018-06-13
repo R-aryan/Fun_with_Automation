d50fe1079492acd0239fb205ca8af93588556fe0
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-13 14:40:53 +0530

    added website list

--- website_blocker.py
+++ website_blocker.py
@@ -1,4 +1,4 @@
 
 hosts_path= r"C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com","fb.com","facebook.com"]
+website_list=["www.facebook.com","fb.com","facebook.com","www.hotmail.com"]
