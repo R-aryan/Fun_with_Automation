63c76cdd847a502e65b51075e33e6f3891e98e6a
Ritesh Raman Aryan <aryan.rritesh@gmail.com>
2018-06-19 15:52:58 +0530

    reading and writing contents from host file

--- website-blocker/website_blocker.py
+++ website-blocker/website_blocker.py
@@ -2,12 +2,17 @@ import time
 from datetime import datetime as dt
 
 hosts_path= r"C:\Windows\System32\drivers\etc\hosts"
+hosts_temp="hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","fb.com","facebook.com","www.hotmail.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16) :
         print("working hours..")
+        with open(hosts_temp,'r+') as file:
+            content=file.read()
+            print(content)
+
     
     else:
         print("fun hours....")
